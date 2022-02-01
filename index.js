9f8e35560b02264ba83426ac8e95097417e70728
Shubham <dusaneshubham>
2022-02-01 18:54:34 +0530

    trying to push new thing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //importing require modules
-const express=require('express');
-const mongoose=require('mongoose');
-const app=express();
-const cors=require('cors');
-const dotenv=require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
 
 
@@ -13,26 +13,29 @@ app.use(cors());
 app.use(express.json());
 
 //All Routes imports
-const userRoutes=require('./src/Routes/user');
-const homeRoutes=require('./src/Routes/home');
+const userRoutes = require('./src/Routes/user');
+const homeRoutes = require('./src/Routes/home');
 
 //MongoDB Connection Configuration
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.cnyo8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.cnyo8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
     console.log('DataBase Connected Successfully');
 }).catch(err => {
-    console.log('DataBase Connection Error: '+err);
+    console.log('DataBase Connection Error: ' + err);
 });
 
 //All Routers Information
-app.use('/',homeRoutes);
+app.use('/', homeRoutes);
 // app.use('/post',post)
-app.use('/user',userRoutes);
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    console.log("xyz file");
+})
 
 //making server on port 5000
-app.listen(port ,()=>{
+app.listen(port, () => {
     console.log(`Server is running cool on ${port}`);
 });
-
